1cac911bc3e64508640c85afa3b0eb710ec867fa
Jom Dollesin <codewithjom@gmail.com>
2023-05-25 19:17:23 +0800

    feat: added simple form.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
+      <form>
+        <div className='bg-white p-2 border space-x-2 flex justify-center items-center'>
+          <input type='text' className='border pl-2' />
+          <input type='text' className='border pl-2' />
+          <button className='border px-2 bg-gray-300'>Send</button>
+        </div>
+      </form>
+      <div>
+        <h1>Data</h1>
+      </div>
     </main>
   )
 }
